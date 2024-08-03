7d7de440415af0d4e229e4e759110881d8a2495e
adimerpaul <adimer101@gmail.com>
2024-08-03 15:59:12 -0400

    colcoado de binding

--- app/src/main/java/com/example/back1/MainActivity.kt
+++ app/src/main/java/com/example/back1/MainActivity.kt
@@ -8,29 +8,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.back1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val num1 = findViewById<EditText>(R.id.num1)
-        val num2 = findViewById<EditText>(R.id.num2)
-        val result = findViewById<TextView>(R.id.result)
-        val add = findViewById<Button>(R.id.add)
-        add.setOnClickListener {
-            val n1 = num1.text.toString()
-            val n2 = num2.text.toString()
+        binding.add.setOnClickListener {
+            val n1 = binding.num1.text.toString()
+            val n2 = binding.num2.text.toString()
             if (n1.isEmpty() || n2.isEmpty()) {
-                result.text = "Please enter two numbers"
+                binding.result.text = "Please enter two numbers"
             } else {
                 val sum = n1.toInt() + n2.toInt()
-                result.text = "Result: $sum"
+                binding.result.text = "Result: $sum"
             }
         }
     }
